090c80f2647e32207b9bcb01c04ef282726beebb
twenty-one-savage <Muravtsev21@gmail.com>
2019-08-14 22:55:12 +0300

    ругается на appendChild

--- js/photo.js
+++ js/photo.js
@@ -2,26 +2,29 @@
 // Зависит от util.js
 // Photo module
 // Генерирует и вставляет фотографии
-(function (newChild) {
+(function () {
   var picture = document.querySelector('.pictures');
   var similarPhotoTemplate = document.querySelector('#picture').content.querySelector('.picture');
   var bigPictureElement = document.querySelector('.big-picture');
   var commentsElement = document.querySelector('.social__comments');
-  var commentsCountElement = document.querySelector('.social__comment-count');
-  var commentsLoaderElement = document.querySelector('.comments-loader');
+  var listComment = bigPictureElement.querySelector('.social__comments');
+  // var commentsCountElement = document.querySelector('.social__comment-count');
+  // var commentsLoaderElement = document.querySelector('.comments-loader');
 
   // Cоздание массива с комментариями
-  var createComments = function (photo) {
+  var createComment = function (photo) {
     var commentField = document.createElement('li');
     commentField.classList.add('social__comment');
     var commentImg = document.createElement('img');
     commentImg.classList.add('social__picture');
-    commentImg.src = 'img/avatar-' + window.util.getRandomInt(1, 6) + '.svg'
+    commentImg.src = 'img/avatar-' + window.util.getRandomInt(1, 6) + '.svg';
     commentImg.alt = 'Аватар комментатора фотографии';
+    commentField.appendChild(commentImg);
     var commentDescription = document.createElement('p');
     commentDescription.classList.add('social__text');
-    commentDescription.textContent = photo.comments.message;
-    commentField.appendChild(commentImg, commentDescription);
+    commentDescription.textContent = photo.message;
+    commentField.appendChild(commentDescription);
+    return commentField;
   };
 
   // Функция для вывода большого изображения
@@ -30,8 +33,18 @@
     bigPictureElement.querySelector('.big-picture__img img').src = photo.url;
     bigPictureElement.querySelector('.likes-count').textContent = photo.likes;
     bigPictureElement.querySelector('.comments-count').textContent = photo.comments.length;
-    bigPictureElement.querySelector('.social__comments').textContent = createComments(photo);
     bigPictureElement.querySelector('.social__caption').textContent = photo.description;
+    renderComments(photo.comments);
+  };
+
+  var renderComments = function (comments) {
+    listComment.innerHTML = '';
+    var fragment = document.createDocumentFragment();
+    comments.forEach(function (element) {
+      var comment = createComment(element);
+      fragment.appendChild(comment);
+    });
+    listComment.appendChild(fragment);
   };
 
   var createPhotos = function (photo) {
